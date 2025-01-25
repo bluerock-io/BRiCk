89394ae999fffaa605e7160dbca6bb2dcafebcff
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2025-01-25 02:24:31 +0100

    Extract alloc_id.v

--- rocq-bluerock-brick/theories/lang/cpp/semantics/ptrs.v
+++ rocq-bluerock-brick/theories/lang/cpp/semantics/ptrs.v
@@ -1,5 +1,5 @@
 (*
- * Copyright (c) 2020-2023 BlueRock Security, Inc.
+ * Copyright (c) 2020-2025 BlueRock Security, Inc.
  * This software is distributed under the terms of the BedRock Open-Source License.
  * See the LICENSE-BedRock file in the repository root for details.
  *)
@@ -21,6 +21,7 @@ Require Import bedrock.prelude.numbers.
 Require Import bedrock.lang.cpp.syntax.
 Require Export bedrock.lang.cpp.semantics.types.
 Require Export bedrock.lang.cpp.semantics.genv.
+Require Export bedrock.lang.cpp.semantics.alloc_id.
 
 (* We only load Iris to declare trivial OFEs over pointers via [leibnizO]. *)
 Require Import iris.algebra.ofe.
@@ -28,18 +29,6 @@ Require Import iris.algebra.ofe.
 #[local] Close Scope nat_scope.
 #[local] Open Scope Z_scope.
 Implicit Types (σ : genv).
-
-(** ** Allocation IDs.
-    We use them to model pointer provenance, following Cerberus.
- *)
-Record alloc_id := MkAllocId { alloc_id_car : N }.
-
-#[global] Instance MkAllocId_inj : Inj (=) (=) MkAllocId.
-Proof. by intros ?? [=]. Qed.
-#[global] Instance alloc_id_eq_dec : EqDecision alloc_id.
-Proof. solve_decision. Qed.
-#[global] Instance alloc_id_countable : Countable alloc_id.
-Proof. by apply: (inj_countable' alloc_id_car MkAllocId) => -[?]. Qed.
 
 Module Type PTRS_SYNTAX_INPUTS.
   Parameter ptr : Set.
