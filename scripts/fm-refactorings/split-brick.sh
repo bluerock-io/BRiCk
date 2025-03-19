601e28935a8570289b3468417bdfd469cd74cca0
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2025-03-19 14:05:59 +0100

    split-brick: choose gsed/sed appropriately

--- scripts/fm-refactorings/split-brick.sh
+++ scripts/fm-refactorings/split-brick.sh
@@ -3,11 +3,14 @@ get_srcs() {
   find . -type f \( -iname '*.v' -o -iname '*.rst' \)
 }
 get_dunes() {
- find . -iname dune
+  find . -iname dune
 }
 
-gsed -i -E 's/bedrock.lang.((bi)|(algebra)|(tactic)|(base_logic)|(si_logic)|(proofmode))/bedrock.iris.extra.\1/g' $(get_srcs)
-gsed -i -E 's/bedrock.lang.cpp.$/bedrock.lang.cpp.cpp./g' $(get_srcs)
-gsed -i -E 's/bedrock.iris.extra.base_logic.mpred_prop/bedrock.lang.cpp.logic.mpred_prop/g' $(get_srcs)
-gsed -i -E 's/bedrock.iris.extra.base_logic.lib.spectra_mpred/bedrock.lang.cpp.logic.spectra_mpred/g' $(get_srcs)
-gsed -i -E 's/bedrock.lang/bedrock.iris.extra bedrock.lang.cpp/g' $(get_dunes)
+SED=gsed
+which -s $SED || SED=sed
+
+$SED -i -E 's/bedrock.lang.((bi)|(algebra)|(tactic)|(base_logic)|(si_logic)|(proofmode))/bedrock.iris.extra.\1/g' $(get_srcs)
+$SED -i -E 's/bedrock.lang.cpp.$/bedrock.lang.cpp.cpp./g' $(get_srcs)
+$SED -i -E 's/bedrock.iris.extra.base_logic.mpred_prop/bedrock.lang.cpp.logic.mpred_prop/g' $(get_srcs)
+$SED -i -E 's/bedrock.iris.extra.base_logic.lib.spectra_mpred/bedrock.lang.cpp.logic.spectra_mpred/g' $(get_srcs)
+$SED -i -E 's/bedrock.lang/bedrock.iris.extra bedrock.lang.cpp/g' $(get_dunes)
