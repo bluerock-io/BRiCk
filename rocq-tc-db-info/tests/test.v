a6385f7da886ef2a97462096362723ea8c9fee24
Gregory Malecha <gregory@bluerock.io>
2025-03-22 07:32:15 -0400

    Add license to the test file.

--- rocq-tc-db-info/tests/test.v
+++ rocq-tc-db-info/tests/test.v
@@ -1,3 +1,9 @@
+(*
+ * Copyright (C) BlueRock Security Inc. 2024
+ *
+ * This software is distributed under the terms of the BedRock Open-Source
+ * License. See the LICENSE-BedRock file in the repository root for details.
+ *)
 Require Import tc_db_info.tc_db_info.
 
 Create HintDb test discriminated.
