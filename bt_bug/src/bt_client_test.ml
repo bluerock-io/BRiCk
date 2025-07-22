a98b36befd85840bb8c00c490b438c171362213c
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2025-07-22 23:56:20 +0200

    Revert "Switch to reproducing with `base`"
    This reverts commit b1cda8b82af4b323f2b840a0849449bb486ed292.

--- bt_bug/src/bt_client_test.ml
+++ bt_bug/src/bt_client_test.ml
@@ -4,7 +4,7 @@ exception Test_exc
 let _ =
   Printf.printf "Printexc.backtrace_status (before using Bt_bug_lib): %b\n" (Printexc.backtrace_status());
   (* ^^ already true! *)
-  let _ = Base.compare_option in
+  let _ = Bt_bug.Bt_bug_lib.test_value in
   (* ^^ Needed to load the library and trigger the bug *)
   Printf.printf "Printexc.backtrace_status (after using Bt_bug_lib): %b\n" (Printexc.backtrace_status());
   raise Test_exc
